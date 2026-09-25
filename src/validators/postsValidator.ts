92d2b37d6e07d102d1aac320214a3ac65a633cbc
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2026-09-25 14:35:45 +0200

    change validator

--- src/validators/postsValidator.ts
+++ src/validators/postsValidator.ts
@@ -19,25 +19,50 @@ export function validatePosts(
   category: unknown,
   tags: unknown
 ) {
-  if (
+  if (!category) {
+    return 'Category is required'
+  } else if (
     typeof category !== 'string' ||
     !allowedCategories.includes(category)
   ) {
     return 'Invalid category';
   }
 
-  if (
+  if (!title) {
+    return 'Title is required'
+  } else  if (
     typeof title !== 'string' ||
     title.trim() === ''
-  ) {
-    return 'Title must be non-empty string';
+    ) {
+      return 'Title must be non-empty string';
+  } else if (title.trim().length > 255) {
+    return 'Title must not exceed 255 characters.'
   }
 
-  if (
+
+  if (!content) {
+    return 'Content is required'
+  } else  if (
     typeof content !== 'string' ||
     content.trim() === ''
   ) {
     return 'Content must be non-empty string';
   }
 
+  if (tags !== undefined) {
+    if (
+      !Array.isArray(tags) ||
+      !tags.every(
+        (tag) => typeof tag === 'string' && tag.trim() !== ''
+      )
+    ) {
+      return 'Tags must be an array of non-empty strings';
+    } 
+    if (tags.some((tag) => tag.trim().length > 20)) {
+      return 'Each tag must not exceed 20 characters.'
+    }
+  }
+  
+
+  return null;
 }
